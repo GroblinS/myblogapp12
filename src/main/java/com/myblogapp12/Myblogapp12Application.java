3807f37154789d07a0464f109afde3db8179eba3
Pushpendra Kushwaha <pk1592098@gmail.com>
2024-03-10 17:14:36 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/myblogapp12/Myblogapp12Application.java
@@ -0,0 +1,13 @@
+package com.myblogapp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblogapp12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblogapp12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblogapp12/Myblogapp12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblogapp12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblogapp12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
