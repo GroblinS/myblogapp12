cb52654dabd7aea1b1e6c4b586c451fa77656d4e
Pushpendra Kushwaha <pk1592098@gmail.com>
2024-03-12 17:48:20 +0530

    127:Stream api3

---
+++ src/main/java/com/myblogapp12/SampleTest.java
@@ -0,0 +1,12 @@
+package com.myblogapp12;
+
+public class SampleTest {
+    public static int test1() {
+        SampleTest sampleTest = new SampleTest();
+        int otp = sampleTest.returnOTp();
+        return otp;
+    }
+    public int returnOTp(){
+        return 123456;
+    }
+}
