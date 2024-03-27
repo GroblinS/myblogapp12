b57117e4bed3e3309847152ed24dcc034c120495
Pushpendra Kushwaha <pk1592098@gmail.com>
2024-03-27 19:31:00 +0530

    127:Stream api3

---
+++ src/main/java/com/myblogapp12/entity/User.java
@@ -0,0 +1,19 @@
+package com.myblogapp12.entity;
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"username"}),
+        @UniqueConstraint(columnNames = {"email"})
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+}
